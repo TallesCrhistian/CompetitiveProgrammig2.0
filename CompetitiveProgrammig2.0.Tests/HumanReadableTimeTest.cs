a4e374bdbeb9d00ccf0250d9dbdc3860a201aee5
Talles Crhistian Arriel <72113346+TallesCrhistian@users.noreply.github.com>
2023-03-05 20:19:48 -0300

    Adicionando HumanReadableTime

---
+++ CompetitiveProgrammig2.0.Tests/HumanReadableTimeTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace CompetitiveProgrammig2.Tests
+{
+    public class HumanReadableTimeTest
+    {
+        HumanReadableTime _humanReadableTime;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _humanReadableTime = new HumanReadableTime();
+        }
+
+        [TestCase(0, "00:00:00")]
+        [TestCase(5, "00:00:05")]
+        [TestCase(60, "00:01:00")]
+        [TestCase(86399, "23:59:59")]
+        [TestCase(359999, "99:59:59")]
+        public void GetReadableTime_(int seconds, string expectedResult)
+        {
+            string result = _humanReadableTime.GetReadableTime(seconds);
+
+            Assert.That(expectedResult == result);
+        }
+    }
+}
---
+++ CompetitiveProgrammig2.0/HumanReadableTime.cs
@@ -0,0 +1,98 @@
+﻿namespace CompetitiveProgrammig2
+{
+    public class HumanReadableTime
+    {
+        public string GetReadableTime(int seconds)
+        {
+            int hours = 0;
+            int minutes = 0;
+            string result = string.Empty;
+
+            if (seconds == 3600)
+            {
+                hours = 1;
+            }
+
+            else
+            {
+                hours = seconds / 3600;
+            }
+
+            if (seconds == 60)
+            {
+                minutes = 1;
+            }
+
+            else
+            {
+                minutes = seconds / 60;
+            }
+
+            if (hours > 0)
+            {
+                if (hours > 9 && hours < 99)
+                {
+                    result = hours.ToString() + ":";
+                }
+
+                else if (hours >= 99)
+                {
+                    result = "99:";
+                }
+
+                else
+                {
+                    result = "0" + hours.ToString() + ":";
+                }
+            }
+
+            else result = "00:";
+
+            minutes -= hours * 60;
+
+            if (minutes > 0)
+            {
+                if (minutes > 9 && minutes < 60)
+                {
+                    result += minutes.ToString() + ":";
+                }
+
+                else if (minutes > 60)
+                {
+                    result += "59:";
+                }
+
+                else
+                {
+                    result += "0" + minutes.ToString() + ":";
+                }
+            }
+
+            else result += "00:";
+
+            seconds -= minutes * 60;
+
+            if (seconds > 0)
+            {
+                if (seconds > 9 && seconds < 60)
+                {
+                    result += seconds.ToString();
+                }
+
+                else if (seconds > 60)
+                {
+                    result += "59";
+                }
+
+                else
+                {
+                    result += "0" + seconds.ToString();
+                }
+            }
+
+            else result += "00";
+
+            return result;
+        }
+    }
+}
