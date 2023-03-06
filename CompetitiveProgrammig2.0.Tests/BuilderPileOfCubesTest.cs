4e1d8e34f108a40a18d70877f8f6c83d6f92751a
Talles Crhistian Arriel <72113346+TallesCrhistian@users.noreply.github.com>
2023-03-05 22:05:08 -0300

    Feature: Adicionando BuildPileOfCubes

---
+++ CompetitiveProgrammig2.0.Tests/BuilderPileOfCubesTest.cs
@@ -0,0 +1,22 @@
+﻿namespace CompetitiveProgrammig2.Tests
+{
+    public class BuilderPileOfCubesTest
+    {
+        BuildPileOfCubes _buildPileOfCubes;   
+
+        [SetUp]
+        public void SetUp()
+        {
+            _buildPileOfCubes= new BuildPileOfCubes();
+        }
+
+        [TestCase(4183059834009, 2022)]
+        [TestCase(24723578342962, -1)]
+        public void FindNb(long m, long expectedResult)
+        {
+            long result = _buildPileOfCubes.findNb(m);
+
+            Assert.That(expectedResult == result);
+        }
+    }
+}
---
+++ CompetitiveProgrammig2.0/BuildPileOfCubes.cs
@@ -0,0 +1,27 @@
+﻿namespace CompetitiveProgrammig2
+{
+    public class BuildPileOfCubes
+    {
+        public long findNb(long m)
+        {
+            long result = 0;
+
+            for (int i = 1; i < m; i++)
+            {
+                double last = Math.Pow(i, 3);
+
+                m = m - (long)last;
+
+                result++;
+            }
+
+            if (m > 0 || m < 0)
+            {
+                return -1;
+            }
+
+            return result;
+        }
+    }
+}
+
