8c6bc62416daae944f25f2b3980370f5574749cc
Talles Crhistian Arriel <72113346+TallesCrhistian@users.noreply.github.com>
2023-03-06 21:35:10 -0300

    Feature: Adicionando TwoSum

---
+++ CompetitiveProgrammig2.0.Tests/TwoSumTest.cs
@@ -0,0 +1,49 @@
+﻿namespace CompetitiveProgrammig2.Tests
+{
+    public class TwoSumTest
+    {
+        TwoSum _twoSum;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _twoSum = new TwoSum();
+        }
+
+        [Test]
+        public void TwoSum_Index()
+        {
+            int[] numbers = new int[] { 1, 2, 3 };
+            int target = 4;
+            int[] expectedResult = new int[] { 0, 2 };
+
+            int[] result = _twoSum.TwoSumMethod(numbers, target);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void TwoSum_Index_TestTwo()
+        {
+            int[] numbers = new int[] { 1234, 5678, 9012 };
+            int target = 14690;
+            int[] expectedResult = new int[] { 1, 2 };
+
+            int[] result = _twoSum.TwoSumMethod(numbers, target);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void TwoSum_Index_TestThree()
+        {
+            int[] numbers = new int[] { 2, 2, 3 };
+            int target = 4;
+            int[] expectedResult = new int[] { 0, 1 };
+
+            int[] result = _twoSum.TwoSumMethod(numbers, target);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
---
+++ CompetitiveProgrammig2.0/TwoSum.cs
@@ -0,0 +1,21 @@
+﻿namespace CompetitiveProgrammig2
+{
+    public class TwoSum
+    {
+        public int[] TwoSumMethod(int[] numbers, int target)
+        {         
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int e = 1; e < numbers.Length; e++)
+                {
+                    if (numbers[i] + numbers[e] == target)
+                    {
+                        return new int[]  {i, e };
+                    }
+                }
+            }
+
+            return new int[0];
+        }
+    }
+}
