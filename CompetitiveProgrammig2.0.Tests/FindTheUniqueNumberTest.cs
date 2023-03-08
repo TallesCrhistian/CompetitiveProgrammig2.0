a6d8cd12cf91cda7bf17260bb4b4ffb0acc2974b
Talles Crhistian Arriel <72113346+TallesCrhistian@users.noreply.github.com>
2023-03-07 22:22:31 -0300

    Feature: Adicionando FindTheUniqueNumber

---
+++ CompetitiveProgrammig2.0.Tests/FindTheUniqueNumberTest.cs
@@ -0,0 +1,25 @@
+﻿
+namespace CompetitiveProgrammig2.Tests
+{
+    public class FindTheUniqueNumberTest
+    {
+        FindTheUniqueNumber _findTheUniqueNumber;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _findTheUniqueNumber = new FindTheUniqueNumber();
+        }
+
+        [Test]
+        public void GetUnique_FindDifferentNumber()
+        {
+            IEnumerable<int> number = new int[] { 11, 11, 14, 11, 11 };
+            int expectedResult = 14;
+
+            int result = _findTheUniqueNumber.GetUnique(number);
+
+            Assert.That(expectedResult == result);
+        }
+    }
+}
---
+++ CompetitiveProgrammig2.0/FindTheUniqueNumber.cs
@@ -0,0 +1,21 @@
+﻿namespace CompetitiveProgrammig2
+{
+    public class FindTheUniqueNumber
+    {
+        public int GetUnique(IEnumerable<int> numbers)
+        {
+            List<int> listInt = numbers.ToList();
+            listInt.Sort();
+
+            if (listInt[0] != listInt[1])
+            {
+                return listInt[0];
+            }
+
+            else
+            {
+                return listInt[listInt.Count - 1];
+            }           
+        }
+    }
+}
