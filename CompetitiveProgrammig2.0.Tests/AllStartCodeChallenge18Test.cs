588082b38104c6c349e30d05f9fe6b27b1225dc7
Talles Crhistian Arriel <72113346+TallesCrhistian@users.noreply.github.com>
2023-03-15 21:11:05 -0300

    Feature: Adicionando AllStarcodeChallenge18

---
+++ CompetitiveProgrammig2.0.Tests/AllStartCodeChallenge18Test.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompetitiveProgrammig2.Tests
+{
+    internal class AllStartCodeChallenge18Test
+    {
+        AllStartCodeChallenge18 _allStartCodeChallenge18;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _allStartCodeChallenge18 = new AllStartCodeChallenge18();
+        }
+
+        [TestCase("Hello", 'o', 1)]
+        public void StrCount_occutencesLetter(string str, char letter, int expectedResult)
+        {
+            int result = _allStartCodeChallenge18.StrCount(str, letter);
+
+            Assert.That(result == expectedResult);
+        }
+    }
+}
---
+++ CompetitiveProgrammig2.0/AllStartCodeChallenge18.cs
@@ -0,0 +1,20 @@
+﻿namespace CompetitiveProgrammig2
+{
+    public class AllStartCodeChallenge18
+    {
+        public int StrCount(string str, char letter)
+        {
+            int result = new int();
+
+            foreach (char index in str)
+            {
+                if (index == letter)
+                {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
