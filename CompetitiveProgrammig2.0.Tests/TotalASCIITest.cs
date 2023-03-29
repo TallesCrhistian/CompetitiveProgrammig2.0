b7dbb6754b474bdb47dc824c470abcf9af149afd
unknown <tallescrhistiandev@gmail.com>
2023-03-28 22:47:22 -0300

    Feature: Add ASCII

---
+++ CompetitiveProgrammig2.0.Tests/TotalASCIITest.cs
@@ -0,0 +1,21 @@
+﻿namespace CompetitiveProgrammig2.Tests
+{
+    public class TotalASCIITest
+    {
+        ASCIITotal _aSCIITotal;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _aSCIITotal = new ASCIITotal();
+        }
+
+        [TestCase("Mary Had A Little Lamb", 1873)]
+        public void UniTotal_Translator(string str, int expectedResult)
+        {
+            int result = _aSCIITotal.UniTotal(str);
+
+            Assert.That(expectedResult == result);
+        }
+    }
+}
---
+++ CompetitiveProgrammig2.0/ASCIITotal.cs
@@ -0,0 +1,22 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Text;
+
+namespace CompetitiveProgrammig2
+{
+    public class ASCIITotal
+    {
+        public int UniTotal(string str)
+        {
+            ulong result = 0;
+
+            byte[] asciiValues = Encoding.ASCII.GetBytes(str);
+
+            for (int i = 0; i < asciiValues.Length; i++)
+            {
+                result += asciiValues[i];
+            }
+
+            return (int)result;
+        }
+    }
+}
