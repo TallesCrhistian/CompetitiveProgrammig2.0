6b7311c636bbfe50125c0f99aedde3ff98ac63eb
Talles Arriel <tallescrhistiandev@gmail.com>
2023-04-25 21:49:13 -0300

    ''

---
+++ CompetitiveProgrammig2.0.Tests/PowerOf2Test.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompetitiveProgrammig2.Tests
+{
+    internal class PowerOf2Test
+    {
+        Power_of_2 _power_Of_2;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _power_Of_2 = new Power_of_2();
+        }
+
+        [TestCase(2)]
+        public void Past_Milliseconds(int n)
+        {
+            BigInteger[] expectedResult = { 1, 2, 4 };
+            BigInteger[] result = _power_Of_2.PowersOfTwo(n);
+
+            Assert.AreEqual(result, expectedResult);
+        }
+    }
+}
---
+++ CompetitiveProgrammig2.0/Power of 2.cs
@@ -0,0 +1,23 @@
+﻿using System.Numerics;
+
+namespace CompetitiveProgrammig2
+{
+    public class Power_of_2
+    {
+        public BigInteger[] PowersOfTwo(int n)
+        {
+            BigInteger[] result = new BigInteger[n + 1];
+            int index = 0;
+
+            for (int i = 0; i < n + 1; i++)
+            {
+                result[index] = (BigInteger)Math.Pow(2 , i);
+                index++;
+            }
+
+            return result;
+
+        }
+
+    }
+}
