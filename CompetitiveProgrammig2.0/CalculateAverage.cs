4dead14091dfcc854c869dada9c8464756ffe88c
Talles Arriel <tallescrhistiandev@gmail.com>
2023-05-30 20:33:54 -0300

    CalculateAverage

---
+++ CompetitiveProgrammig2.0/CalculateAverage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompetitiveProgrammig2
+{
+    internal class CalculateAverage
+    {
+        public double FindAverage(double[] array)
+        {
+            double result = 0;
+
+            if (array.Length > 0)
+            {
+                foreach (var item in array)
+                {
+                    result += item;
+                }
+
+                return result / array.Length;
+            }
+
+            return 0;
+        }
+    }
+}
